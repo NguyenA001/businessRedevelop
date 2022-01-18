bd9c1dd38a05d6890b6b6c3cdc7c3b93e7b6f5b9
NguyenA001 <annguyen8545@gmail.com>
2022-01-17 19:42:45 -0800

    fixed links

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,18 @@
 let facebook = document.getElementById('logoLINK');
 let twitter = document.getElementById('logoLINK2');
-
-console.log(facebook);
+let fb = document.getElementById('logoLINK3');
+let tw = document.getElementById('logoLINK3');
 
 facebook.addEventListener('click',function(e){
     window.open('https://www.facebook.com/bunthaispices/', '_blank');
 });
 
 twitter.addEventListener('click',function(e){
+    window.open('https://twitter.com/ThaiSpicesLodi', '_blank');
+});
+fb.addEventListener('click',function(e){
+    window.open('https://www.facebook.com/bunthaispices/', '_blank');
+});
+tw.addEventListener('click',function(e){
     window.open('https://twitter.com/ThaiSpicesLodi', '_blank');
 });
