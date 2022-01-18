50d7ca69f0d25c77d634c6fb9116844bdf6831de
NguyenA001 <annguyen8545@gmail.com>
2022-01-17 19:45:08 -0800

    twitter link

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 let facebook = document.getElementById('logoLINK');
 let twitter = document.getElementById('logoLINK2');
 let fb = document.getElementById('logoLINK3');
-let tw = document.getElementById('logoLINK3');
+let tw = document.getElementById('logoLINK4');
 
 facebook.addEventListener('click',function(e){
     window.open('https://www.facebook.com/bunthaispices/', '_blank');
